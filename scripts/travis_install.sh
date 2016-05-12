f06528422007395cb22d4d77b37580e893069fca
Maurice-Pascal Sonnemann <mpsonnemann@gmail.com>
2016-05-13 01:38:44 +0200

    Migrate Travis CI to container-based variant

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -1,4 +1,8 @@
 set -e
 apt-get update -y
-apt-get install -y clang libswscale2 libswscale-dev libavdevice-dev libavformat-dev libgd2-noxpm libgd2-noxpm-dev
+apt-get install -y cmake clang
+apt-get install -y libswscale2 libswscale-dev
+apt-get install -y ffmpeg
+apt-get install -y libavdevice-dev libavformat-dev libavcodec-dev libavfilter-dev libavutil-dev libavresample-dev
+apt-get install -y libgd-dev
 apt-get install -y libwxgtk2.8-dev
